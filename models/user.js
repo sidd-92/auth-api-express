643b802f4b20a2114efb835193de7843e0793e04
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2022-06-18 22:09:39 +0530

    Added More fields in user schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+
 const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true, default: false },
+	_id: mongoose.Schema.Types.ObjectId,
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	isAdmin: { type: Boolean, required: true, default: false },
+	browserLoggedIn: { type: String, default: null },
+	isLoggedIn: { type: Boolean, default: false },
+	lastLoggedIn: { type: Date, default: null },
 });
 
 module.exports = mongoose.model("User", userSchema);
