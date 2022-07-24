c56f42e6eabb1e36330196a42d3fe09b454d501c
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2022-07-24 17:44:42 +0530

    Added Google Sign In

--- models/user.js
+++ models/user.js
@@ -3,10 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	email: { type: String, required: true },
-	password: { type: String, required: true },
+	password: {
+		type: String,
+		required: function () {
+			return this.googleSignUp == true ? false : true;
+		},
+	},
 	isAdmin: { type: Boolean, required: true, default: false },
 	browserLoggedIn: { type: String, default: null },
 	isLoggedIn: { type: Boolean, default: false },
+	googleSignUp: { type: Boolean, default: false },
 	lastLoggedIn: { type: Date, default: null },
 });
 
